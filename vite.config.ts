a6eece629c7dc11f29b18950d1c919628ff537b3
ZABETH <karta1862025>
2025-07-31 17:09:10 +0800

    [Add]Add base project name.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
+  base: '/GitHub-deploy-test/',
   build: { outDir: 'docs' },
   resolve: {
     alias: {
